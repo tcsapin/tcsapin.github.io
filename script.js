2dbf87159ad2429f5e440a2a7e94cbed0577019f
Andreas Piniais <andreaspiniais.tcs@outlook.com>
2023-10-25 17:11:30 +0300

    Update grid container

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ function myFunction(x) {
     }
     
   }
+
+/* Creating the grid */
+// function grid(el) {
+//     var container = document.createElement("div");
+//     container.id = "main";
+//     container.className = "container";
+
+//     for (i=0; i<16; i+=1) {
+//         var row = document.createElement("div");
+//         row.className = "row";
+//         row.id = "row" + i;
+      
+//         for (k=0; k<16; k+=1) {
+//             var box = document.createElement("div"); 
+//             box.className = "box";
+//             row.appendChild(box);
+//         };
+      
+//         container.appendChild(row);      
+//     };
+  
+//     el.appendChild(container);
+// };
+
+// grid(document.body);
